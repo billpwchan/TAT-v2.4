a673ae7a3a20b13db385be5b2dd4516bd7e9b363
Bill Chan <billpwchan@hotmail.com>
2018-10-09 14:44:02 +0800

    Baseline Draft Completed. Destroy process after 2000 sec.
    Signed-off-by: Bill Chan <billpwchan@hotmail.com>

--- Backup Files(Old Version)/src/script/IEC104InitConnection.java
+++ Backup Files(Old Version)/src/script/IEC104InitConnection.java
@@ -6,10 +6,7 @@ import DB.Script;
 import controller.util.CommonFunctions;
 import engine.Result;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -26,9 +23,6 @@ public class IEC104InitConnection implements InterfaceScript {
     public Result run(ArrayList<ParametersExecution> parameters, HashMap hashMap) throws Exception {
         String ip = parameters.get(1).getValue().trim();    //Not used for simulator monitor process
         int port = ((int) Double.parseDouble(parameters.get(2).getValue().trim()));
-//        int port = 2404;
-        int asduAddress = ((int) Double.parseDouble(parameters.get(3).getValue().trim()));
-//        int asduAddress = 7;
         CommonFunctions.debugLog.debug("Working Directory = " + System.getProperty("user.dir"));
         Path inputPath = Paths.get("src\\script\\IEC104Simulator\\IEC104slavetemplate.ini");
         Path outputPath = Paths.get("IEC104slave.ini");
@@ -39,26 +33,15 @@ public class IEC104InitConnection implements InterfaceScript {
             for (int i = 0; i < lines.size(); i++) {
                 if (lines.get(i) != null && (lines.get(i)).startsWith("Port")) {
                     lines.set(i, "Port=" + port);
-                } else if (lines.get(i) != null && (lines.get(i)).startsWith("AsduAddress")) {
-                    lines.set(i, "AsduAddress=" + asduAddress);
                 }
             }
-
             //Write adjusted configuration to OutputFile.
             Files.write(outputPath, lines, Charset.defaultCharset());
         } catch (IOException ex) {
             CommonFunctions.debugLog.error("Cannot locate the file specified. Please check the IEC104Simulator Folder inside /scripts.", ex);
         }
 
-        Runtime.getRuntime().exec("attrib +H IEC104slave.ini");     //Hide the .ini file from the user.
-        Process process = new ProcessBuilder("src\\script\\IEC104Simulator\\20171117_104Slave.exe").start();
-        InputStream is = process.getInputStream();
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }
+
 //        process.destroy();
         return null;
     }
---
+++ Backup Files(Old Version)/src/script/TriggerIEC104Point.java
@@ -0,0 +1,149 @@
+package script;
+
+import DB.Parameters;
+import DB.ParametersExecution;
+import DB.Script;
+import controller.util.CommonFunctions;
+import engine.Result;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class TriggerIEC104Point implements InterfaceScript {
+    private int register;
+
+    private double value;
+
+    private String dcType;
+
+    private int asduAddress;
+
+    private String stringValue;
+
+    private List<String> lines;
+
+
+    @Override
+    public Result run(ArrayList<ParametersExecution> parameters, HashMap hashMap) throws Exception {
+
+        this.register = ((int) Double.parseDouble(parameters.get(1).getValue().trim()));
+        this.stringValue = (parameters.get(2).getValue().trim());
+        this.value = (int) Double.parseDouble(this.stringValue);
+        this.dcType = parameters.get(3).getValue().trim();
+        this.asduAddress = (int) Double.parseDouble(parameters.get(4).getValue().trim());
+
+        this.lines = IEC104InitConnection.lines.stream().collect(Collectors.toList());
+        try {
+            for (int i = 0; i < lines.size(); i++) {
+                if (lines.get(i) != null && lines.get(i).startsWith("Range0")) {
+                    lines.set(i, "Range0=" + this.adjustedParam().get(0) + ",0x" + Integer.toHexString(this.register) + ",0x0010,BACKGROUND");
+                } else if (lines.get(i) != null && (lines.get(i)).startsWith("AsduAddress")) {
+                    lines.set(i, "AsduAddress=" + asduAddress);
+                } else if (lines.get(i) != null && lines.get(i).startsWith("TimeStamp")) {
+                    lines.set(i, "TimeStamp=0");    //Require confirmation for SOE1.
+                } else if (lines.get(i) != null && lines.get(i).startsWith("PointEnabled")) {
+                    lines.set(i, "PointEnabled0=1");
+                    lines.set(i + 1, "PointType0=" + this.adjustedParam().get(0));
+                    lines.set(i + 2, "PointIOA0=" + this.register);
+                    lines.set(i + 3, "PointValue0=" + this.value);
+                    lines.set(i + 4, "PointRepeat0=1");
+                } else if (lines.get(i) != null && lines.get(i).startsWith("RandomPointEnabled")) {
+                    lines.set(i, "RandomPointEnabled0=0");
+                } else if (lines.get(i) != null && lines.get(i).startsWith("equipmentaddr")) {
+                    lines.set(i, "equipmentaddr0=0");
+                }
+            }
+            this.lines.forEach(CommonFunctions.debugLog::debug);
+            try (BufferedWriter writer = Files.newBufferedWriter(Paths.get("IEC104slave.ini"), Charset.forName("utf-8"))) {
+                for (String line : this.lines) {
+                    writer.write(line, 0, line.length());
+                    writer.newLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+//            Files.write(Paths.get("IEC104slave.ini"), lines, Charset.defaultCharset());
+
+            Runtime.getRuntime().exec("attrib +H IEC104slave.ini");     //Hide the .ini file from the user.
+            Process process = new ProcessBuilder("src\\script\\IEC104Simulator\\20171117_104Slave.exe").start();
+//            InputStream is = process.getInputStream();
+//            InputStreamReader isr = new InputStreamReader(is);
+//            BufferedReader br = new BufferedReader(isr);
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                System.out.println(line);
+//            }
+            Thread.sleep(2000);
+            process.destroy();
+            return null;
+        } catch (IndexOutOfBoundsException | IOException ex) {
+            CommonFunctions.debugLog.error("Invalid Line Index Caught. Please re-start the program.");
+            return null;
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public ArrayList<Parameters> parameters() {
+        return null;
+    }
+
+    @Override
+    public Script scriptInfos() {
+        return null;
+    }
+
+    private ArrayList<String> adjustedParam() {
+        ArrayList<String> param = new ArrayList<>();
+        switch (this.dcType) {
+            case "SOE1":
+            case "DI1": {
+                param.addAll(Arrays.asList("MSPNA"));
+                break;
+            }
+            case "SOE2":
+            case "DI2": {
+                param.addAll(Arrays.asList("MDPNA"));
+                break;
+            }
+            case "AI": {
+                param.addAll(Arrays.asList("MMENC"));
+                break;
+            }
+            default: {
+                CommonFunctions.debugLog.error("Invalid IEC-104 Function Code discovered.");
+                return null;
+            }
+        }
+        return param;
+    }
+
+    public static void main(String[] args) {
+        //Must initiate IEC104 connection to keep lines of IEC104Template as static.
+        IEC104InitConnection temp = new IEC104InitConnection();
+        try {
+            temp.run(null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        TriggerIEC104Point temp1 = new TriggerIEC104Point();
+        try {
+            temp1.run(null, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
