71421a102b23c8c80550ff8fef7a5f68b98a18b7
Bill Chan <billpwchan@hotmail.com>
2019-07-06 17:45:31 +0800

    [General] Output Hex Digits Success

--- src/script/ReceiveIEC104Point.java
+++ src/script/ReceiveIEC104Point.java
@@ -9,6 +9,7 @@ import engine.Result;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.time.Instant;
@@ -18,17 +19,29 @@ import java.util.stream.Collectors;
 
 public class ReceiveIEC104Point implements InterfaceScript {
 
+    private int DOCodeOffset1 = 9;
+    private int DOCodeOffset2 = 12;
+
     @Override
     public Result run(ArrayList<ParametersExecution> parameters, HashMap hashMap) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(IEC104InitConnection.process.getInputStream()));
         String line123;
-        Instant before = Instant.now();
-        while ((line123 = br.readLine()) != null && Duration.between(before, Instant.now()).toMillis() < 10000) {
+        System.out.println("Receiving Points Now");
+        while ((line123 = br.readLine()) != null && !line123.contains("Single Command Response")) {
             //Should be finished initialization
             CommonFunctions.debugLog.debug(line123);
             System.out.println(line123);
         }
-
+        System.out.println("Received DO Command with message: " + line123);
+        for (int i = 0; i < 8; i++) {
+            line123 = br.readLine();
+            System.out.println("Line " + ++i + ": " + line123);
+        }
+        line123 = br.readLine();  //This is the line containing information;
+        System.out.println("Received DO Code Line: " + line123);
+        System.out.println("Last 2 digits: " + line123.substring(line123.length()-3));
+        System.out.println("Central 2 digits: " + line123.substring(line123.length() - DOCodeOffset1, line123.length() - DOCodeOffset1 + 2));
+        System.out.println("Right 2 digits: " + line123.substring(line123.length() - DOCodeOffset2, line123.length() - DOCodeOffset2 + 2));
         return null;
     }
 
@@ -47,12 +60,39 @@ public class ReceiveIEC104Point implements InterfaceScript {
         return null;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Must initiate IEC104 connection to keep lines of IEC104Template as static.
-        IEC104InitConnection temp = new IEC104InitConnection();
+        Path inputPath = Paths.get("src\\script\\IEC104Simulator\\IEC104slavetemplate.ini");
+        Path outputPath = Paths.get("IEC104slave.ini");
         try {
-            temp.run(null, null);
-        } catch (Exception e) {
+            //Read Template File
+            List<String> lines = Files.readAllLines(inputPath);
+            int port = 2404;
+            int asduAddress = 7;
+            for (int i = 0; i < lines.size(); i++) {
+                if (lines.get(i) != null && (lines.get(i)).startsWith("Port")) {
+                    lines.set(i, "Port=" + port);
+                } else if (lines.get(i) != null && (lines.get(i)).startsWith("AsduAddress")) {
+                    lines.set(i, "AsduAddress=" + asduAddress);
+                }
+            }
+            //Write adjusted configuration to OutputFile.
+            Files.write(outputPath, lines, Charset.defaultCharset());
+        } catch (IOException ex) {
+            CommonFunctions.debugLog.error("Cannot locate the file specified. Please check the IEC104Simulator Folder inside /scripts.", ex);
+        }
+        Runtime.getRuntime().exec("attrib +H IEC104slave.ini");     //Hide the .ini file from the user.
+        Process process = new ProcessBuilder("src\\script\\IEC104Simulator\\20171117_104Slave.exe").start();
+        IEC104InitConnection.process = process;
+        InputStream is = process.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String line123;
+        Instant before = Instant.now();
+        try {
+            while (!((line123 = br.readLine()) != null && !line123.contains("Supervisory") && Duration.between(before, Instant.now()).toMillis() < 10000))
+                CommonFunctions.debugLog.debug(line123);
+        } catch (IOException e) {
             e.printStackTrace();
         }
         ReceiveIEC104Point temp1 = new ReceiveIEC104Point();
